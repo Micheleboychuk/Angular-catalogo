5aad457e67c4a788f3a0cd794014bcd9f3635912
Mykhaylo <michele.boychuk@gmail.com>
2023-05-16 22:36:56 +0200

    minor improvment

--- src/app/features/catalog/catalog.component.ts
+++ src/app/features/catalog/catalog.component.ts
@@ -5,27 +5,25 @@ import { CatalogService } from './services/catalog.service';
   selector: 'mb-catalog',
   template: `
     <mb-catalog-form
-      [active]="this.service.active"
-      (save)="this.service.save($event)"
-      (reset)="this.service.reset()"
+      [active]="service.active"
+      (save)="service.save($event)"
+      (reset)="service.reset()"
     >
     </mb-catalog-form>
 
     <hr>
 
     <mb-catalog-list
-        [devices]="this.service.devices"
-        [active]="this.service.active"
-        (setActive)="this.service.setActive($event)"
-        (delete)="this.service.deleteHandler($event)"
+        [devices]="service.devices"
+        [active]="service.active"
+        (setActive)="service.setActive($event)"
+        (delete)="service.deleteHandler($event)"
         >
     </mb-catalog-list>
   `,
 })
 export class CatalogComponent {
-  service: CatalogService;
-  constructor(service: CatalogService) {
-    this.service = service;
-    this.service.getAll();
+  constructor(public service: CatalogService) {
+     service.getAll();
   }
 }
